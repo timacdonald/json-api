e005b44a98b3ad1c8a973fa7e7ec3ba8fccec934
Tim MacDonald <hello@timacdonald.me>
2021-10-04 17:52:32 +1100

    wip

--- src/Support/Fields.php
+++ src/Support/Fields.php
@@ -14,26 +14,28 @@ class Fields
 {
     public static function parse(Request $request, string $resourceType): ?array
     {
-        $typeFields = $request->query('fields') ?? [];
+        return once(function () use ($request, $resourceType): ?array {
+            $typeFields = $request->query('fields') ?? [];
 
-        if (is_string($typeFields)) {
-            throw new HttpException(400, 'The fields parameter must be an array of resource types.');
-        }
+            if (is_string($typeFields)) {
+                throw new HttpException(400, 'The fields parameter must be an array of resource types.');
+            }
 
-        if (! array_key_exists($resourceType, $typeFields)) {
-            return null;
-        }
+            if (! array_key_exists($resourceType, $typeFields)) {
+                return null;
+            }
 
-        $fields = $typeFields[$resourceType];
+            $fields = $typeFields[$resourceType];
 
-        if ($fields === null) {
-            return [];
-        }
+            if ($fields === null) {
+                return [];
+            }
 
-        if (! is_string($fields)) {
-            throw new HttpException(400, 'The fields parameter value must be a comma seperated list of attributes.');
-        }
+            if (! is_string($fields)) {
+                throw new HttpException(400, 'The fields parameter value must be a comma seperated list of attributes.');
+            }
 
-        return explode(',', $fields);
+            return explode(',', $fields);
+        });
     }
 }
--- src/Support/Includes.php
+++ src/Support/Includes.php
@@ -15,17 +15,19 @@ class Includes
 {
     public static function parse(Request $request, string $prefix): Collection
     {
-        $includes = $request->query('include') ?? '';
+        return once(function () use ($request, $prefix): Collection {
+            $includes = $request->query('include') ?? '';
 
-        if (is_array($includes)) {
-            throw new HttpException(400, 'The include parameter must be a comma seperated list of relationship paths.');
-        }
+            if (is_array($includes)) {
+                throw new HttpException(400, 'The include parameter must be a comma seperated list of relationship paths.');
+            }
 
-        return Collection::make(explode(',', $includes))
+            return Collection::make(explode(',', $includes))
             ->mapInto(Stringable::class)
             ->when($prefix !== '', function (Collection $includes) use ($prefix): Collection {
                 return $includes->filter(fn (Stringable $include) => $include->startsWith($prefix));
             })
             ->map(fn (Stringable $include): string => (string) $include->after($prefix)->before('.'));
+        });
     }
 }

d8f583f00ef69e0fecaa0f9c054b7357d58e9ec3
Tim MacDonald <hello@timacdonald.me>
2021-11-01 20:11:48 +1100

    support earlier Laravel verisons

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use Spatie\Once\Cache;
 
 class TestCase extends BaseTestCase
 {
-    use LazilyRefreshDatabase;
+    use RefreshDatabase;
 
     public function tearDown(): void
     {
